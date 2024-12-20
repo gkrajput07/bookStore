826cd7a4626f2fd458e0416c297a8fdfa48102c0
Gopal Chouhan <gkrajput9450@gmail.com>
2024-12-20 20:01:15 +0530

    Frontend Done !

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Home from './Home/Home'
+import { Route, Routes } from "react-router-dom";
+import Courses from './courses/Courses';
+import Signup from './components/Signup';
+
+
+export default function App() {
+  return (
+    <>
+     <Routes>
+       <Route path='/' element={<Home/>}/>
+       <Route path='/course' element={<Courses/>}/>
+       <Route path='/signup' element={<Signup/>}/>
+     </Routes>
+    </>
+  )
+}
